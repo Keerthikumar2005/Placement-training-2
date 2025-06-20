4abfd53398ed273ebf567c38f723d463468bd671
keerthikumar <keerthi200329@gmail.com>
2025-06-20 11:53:58 +0530

    Create filewriting.java

---
+++ 20-06-2025/filewriting.java
@@ -0,0 +1,15 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToFile {
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("output.txt");
+            writer.write("Hello from Java FileWriter!");
+            writer.close();
+            System.out.println("File written successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
+    }
+}
